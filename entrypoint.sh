ea863962d8a61ff4fdf9629dbebeb3d06f814fbc
Ben Navetta <ben.navetta@gmail.com>
2020-03-01 12:07:32 -0800

    Install package dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,15 @@ if [[ ! -z "$1" ]]; then
 fi
 
 echo "Running makepkg from $PWD"
+
+pkg_deps=$(source ./PKGBUILD && echo $makedepends[@] $depends[@])
+echo "Installing dependencies: $pkg_deps"
+pacman -Syu $pkg_deps
+
 chown -R archbuilder "$PWD"
 
 # TODO: support extra flags
 
-su archbuilder -c makepkg
+su archbuilder -c makepkg -f
 
 namcap -i *.pkg.tar.xz
